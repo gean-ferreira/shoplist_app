2b3d05738455c6f88c5196927d1fc914a83ef9d2
Gean Ferreira <gean_roberto@hotmail.com>
2024-09-03 16:01:08 -0300

    feat: Add func de verificação de permissões para garantir que apenas o usuário autenticado possa acessar recursos específicos

--- server/app/security/dependencies.py
+++ server/app/security/dependencies.py
@@ -1,4 +1,4 @@
-from fastapi import Request
+from fastapi import Depends, HTTPException, Request
 from .auth_handler import decode_access_token
 
 
@@ -7,3 +7,15 @@ def get_current_user(request: Request):
     access_token = request.cookies.get("access_token")
     payload = decode_access_token(access_token)
     return int(payload["sub"])
+
+
+# Função para verificar se um valor específico corresponde ao valor do usuário autenticado
+async def verify_user_permission(
+    resource_id: int, authenticated_user_id: int = Depends(get_current_user)
+):
+    print(resource_id, authenticated_user_id)
+    # Verifica se o ID autenticado é igual ao ID do recurso
+    if authenticated_user_id != resource_id:
+        raise HTTPException(
+            status_code=403, detail="Você não tem permissão para acessar este recurso"
+        )
