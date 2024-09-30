6a738e4965e950bb57231779e7ded31ea3e7db89
Gean Ferreira <gean_roberto@hotmail.com>
2024-09-30 10:12:17 -0300

    feat: Add menu lateral

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -2,9 +2,25 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar class="q-px-sm">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
         <q-toolbar-title> Lista de Compras App </q-toolbar-title>
       </q-toolbar>
     </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Barra de navegação </q-item-label>
+      </q-list>
+    </q-drawer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -12,7 +28,19 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink, {
+  EssentialLinkProps,
+} from 'components/EssentialLink.vue';
+import { routes } from 'src/router/routes';
+
 defineOptions({
   name: 'MainLayout',
 });
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
 </script>
